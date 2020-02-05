71166e49c5660f2caa629faa1336b7148a8fac52
Amadeus9029 <965720890@qq.com>
2020-02-05 11:16:33 +0800

    master

--- src/components/Dialog/RegisterDialog.vue
+++ src/components/Dialog/RegisterDialog.vue
@@ -10,14 +10,11 @@
         label-width="100px"
         class="demo-ruleForm"
       >
-        <el-form-item label="密码" prop="pass">
-          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        <el-form-item label="用户名" prop="name">
+          <el-input v-model="ruleForm.name"></el-input>
         </el-form-item>
-        <el-form-item label="确认密码" prop="checkPass">
-          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="年龄" prop="age">
-          <el-input v-model.number="ruleForm.age"></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="submitForm('ruleForm')">注册</el-button>
@@ -31,52 +28,25 @@
 export default {
   name: "RegisterDialog",
   data() {
-    var checkAge = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("年龄不能为空"));
+    var validateName = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
       }
-      setTimeout(() => {
-        if (!Number.isInteger(value)) {
-          callback(new Error("请输入数字值"));
-        } else {
-          if (value < 18) {
-            callback(new Error("必须年满18岁"));
-          } else {
-            callback();
-          }
-        }
-      }, 1000);
     };
     var validatePass = (rule, value, callback) => {
       if (value === "") {
         callback(new Error("请输入密码"));
-      } else {
-        if (this.ruleForm.checkPass !== "") {
-          this.$refs.ruleForm.validateField("checkPass");
-        }
-        callback();
-      }
-    };
-    var validatePass2 = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm.pass) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
       }
     };
     return {
       centerDialogVisible: false,
       ruleForm: {
-        pass: "",
-        checkPass: "",
-        age: ""
+        name: "",
+        password: ""
       },
       rules: {
-        pass: [{ validator: validatePass, trigger: "blur" }],
-        checkPass: [{ validator: validatePass2, trigger: "blur" }],
-        age: [{ validator: checkAge, trigger: "blur" }]
+        name: [{ validator: validateName, trigger: "blur" }],
+        password: [{ validator: validatePass, trigger: "blur" }]
       }
     };
   },
--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
+// https://webpack.js.org/guides/dependency-management/#requirecontext
+const modulesFiles = require.context('./modules', true, /\.js$/)
+// you do not need `import app from './modules/app'`
+// it will auto require all vuex module from modules file
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  // set './app.js' => 'app'
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value.default
+  return modules
+}, {})
 
-export default new Vuex.Store({
-    state: {
-        count: 0,
-        count2: 2,
-        todos: [
-            { id: 1, text: '...', done: true },
-            { id: 2, text: '...', done: false }
-        ]
-    },
-    getters: {
-        doneTodos: state => {
-            return state.todos.filter(todo => todo.done)
-        }
-    },
-    mutations: {
-        increment(state) {
-            // mutate state
-            state.count++
-        }
-    },
-    actions: {
-        increment(context) {
-            context.commit('increment')
-        }
-    }
+const store = new Vuex.Store({
+  modules,
+  getters
 })
+
+export default store
 
