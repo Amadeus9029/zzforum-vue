469a7a8efa475652bce3cf60682283cb759dd811
Amadeus9029 <965720890@qq.com>
2020-02-08 20:56:10 +0800

    master

--- src/components/NavMenu/NavMenu.vue
+++ src/components/NavMenu/NavMenu.vue
@@ -9,6 +9,9 @@
       <el-menu-item>
         <el-image style="width: 48px; height: 48px" :src="url" :fit="fits[0]"></el-image>
       </el-menu-item>
+      <router-link tag="el-menu-item" index="1" :to="{path:'/user'}">用户中心</router-link>
+      <router-link tag="el-menu-item" index="2" :to="{path:'/content'}">文件管理</router-link>
+      <router-link tag="el-menu-item" index="3" :to="{path:'/work'}">文件编辑</router-link>
       <el-menu-item style="float:right">
         <register-dialog></register-dialog>
       </el-menu-item>
@@ -30,13 +33,13 @@ export default {
     return {
       activeIndex: "1",
       fits: ["fill", "contain", "cover", "none", "scale-down"],
-      url: "./static/logo.png",
+      url: "./static/logo.png"
     };
   },
   computed: mapState({
     count: state => state.count,
     count2: state => state.count2,
-    count3: (state,getters) => getters.doneTodos
+    count3: (state, getters) => getters.doneTodos
   }),
   methods: {
     handleSelect(key, keyPath) {}
