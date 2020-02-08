2759d95ec793efc7d7a1c1e2cdaf6341c04e9ec9
Amadeus9029 <965720890@qq.com>
2020-02-08 18:24:24 +0800

    master

--- src/components/NavMenu/ToolMenu.vue
+++ src/components/NavMenu/ToolMenu.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="tool-menu">
-      <el-row style="height:50px;line-height:50px;background-color:#ccc;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)">
-          <el-col>菜单</el-col>
-      </el-row>
+    <el-row style="height:50px;line-height:50px;background-color:#ccc;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)">
+      <el-col>
+        <el-dropdown trigger="click">
+          <el-button type="primary">新建</el-button>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-document">新建文件</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-folder">新建文件夹</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {
-  name: "ToolMenu"
-};
+  export default {
+    name: "ToolMenu"
+  };
 </script>
 <style scoped>
 </style>
--- src/views/content/content.vue
+++ src/views/content/content.vue
@@ -11,27 +11,75 @@
       </el-col>
     </el-row>
     <el-row style="height:calc(100vh - 61px);">
-      <el-col :span="5" style="height:calc(100vh - 61px);background:gray;">Aside</el-col>
-      <el-col :span="19">Main</el-col>
+      <el-col :span="5" style="height:calc(100vh - 61px);background:gray;">
+        <div>我的文档</div>
+        <div>最近编辑</div>
+        <div>回收站</div>
+      </el-col>
+      <el-col :span="19">
+        <el-row type="flex" justify="start" style="margin-top: 20px">
+          <el-col class="userSource" :span="3" v-for="item in userSource" :key="item">
+            <el-card shadow="hover">
+              <i class="el-icon-s-tools" style="float:right"></i>
+              <div>
+                <i :class="item.icon" style="font-size:50px"></i>
+              </div>
+              <span>{{item.name}}</span></el-card>
+          </el-col>
+        </el-row>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import NavMenu from "@/components/NavMenu/NavMenu";
-import ToolMenu from "@/components/NavMenu/ToolMenu";
-import Tab from "@/components/Tab/Tab";
-export default {
-  name: "Content",
-  components: {
-    Tab,
-    NavMenu,
-    ToolMenu
-  }
-};
+  import NavMenu from "@/components/NavMenu/NavMenu";
+  import ToolMenu from "@/components/NavMenu/ToolMenu";
+  import Tab from "@/components/Tab/Tab";
+
+  export default {
+    name: "Content",
+    components: {
+      Tab,
+      NavMenu,
+      ToolMenu
+    },
+    data() {
+      return {
+        userSource: [
+          {
+            type: "folder",
+            name: "我的文件",
+            icon: "el-icon-folder"
+          },
+          {
+            type: "folder",
+            name: "aaa",
+            icon: "el-icon-folder"
+          },
+          {
+            type: "folder",
+            name: "数学",
+            icon: "el-icon-folder"
+          },
+          {
+            type: "document",
+            name: "我的文档",
+            icon: "el-icon-document"
+          }
+        ]
+      }
+    }
+  };
 </script>
 <style scoped>
-.el-header {
-  padding: 0;
-}
+  .el-header {
+    padding: 0;
+  }
+  .userSource {
+    padding:10px
+  }
+  .userSource:first-child{
+
+  }
 </style>
