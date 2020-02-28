e78f2c2b9d2de9a551592ce7d47bf4022f198f27
Amadeus9029 <965720890@qq.com>
2020-02-28 16:58:11 +0800

    master

--- src/views/user/userFollows/index.vue
+++ src/views/user/userFollows/index.vue
@@ -18,7 +18,7 @@
               <div>用户名</div>
             </el-col>
             <el-col align="left" style="margin-top:20px">
-              <div>这个人啥都没有写</div>
+              <div>这个人啥都</div>
             </el-col>
           </el-row>
         </el-col>
